253a6ff7be17989e28abedcc0b34f591023d5800
Chris <211069418+chris-mech@users.noreply.github.com>
2026-07-22 23:10:32 +0100

    feat: add IEndpoint pattern with assembly-scanned registration

---
+++ backend/src/HalcyonRecords.Api/Common/Endpoints/EndpointRegistrationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace HalcyonRecords.Api.Common.Endpoints;
+
+public static class EndpointRegistrationExtensions
+{
+    public static IServiceCollection AddEndpoints(
+        this IServiceCollection services,
+        Assembly assembly
+    )
+    {
+        var serviceDescriptors = assembly
+            .DefinedTypes.Where(type =>
+                type is { IsAbstract: false, IsInterface: false }
+                && type.IsAssignableTo(typeof(IEndpoint))
+            )
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
+
+        services.TryAddEnumerable(serviceDescriptors);
+
+        return services;
+    }
+
+    public static WebApplication MapEndpoints(this WebApplication app)
+    {
+        var endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        foreach (var endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(app);
+        }
+
+        return app;
+    }
+}
