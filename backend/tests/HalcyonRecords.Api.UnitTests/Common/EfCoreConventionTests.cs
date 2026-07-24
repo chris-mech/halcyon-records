30717312f540fa9f649ed3a11624e846c37fbe45
Chris <211069418+chris-mech@users.noreply.github.com>
2026-07-24 16:32:27 +0100

    test: add HalcyonRecords.Api.UnitTests with EfCoreConventionTests and wire CI

---
+++ backend/tests/HalcyonRecords.Api.UnitTests/Common/EfCoreConventionTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using HalcyonRecords.Api.Infrastructure;
+
+namespace HalcyonRecords.Api.UnitTests.Common;
+
+public class EfCoreConventionTests
+{
+    [Fact]
+    public void Model_ShouldNotHaveAnyEagerLoadedNavigations()
+    {
+        using var dbContext = new ApplicationDbContextFactory().CreateDbContext([]);
+
+        var eagerLoadedNavigations = dbContext
+            .Model.GetEntityTypes()
+            .SelectMany(entityType => entityType.GetNavigations())
+            .Where(navigation => navigation.IsEagerLoaded)
+            .Select(navigation =>
+                $"{navigation.DeclaringEntityType.ClrType.Name}.{navigation.Name}"
+            )
+            .ToList();
+
+        eagerLoadedNavigations
+            .Should()
+            .BeEmpty(
+                "no navigation should use .AutoInclude() - list-query slices must project explicitly via .Select() to avoid cartesian-product bugs"
+            );
+    }
+}
