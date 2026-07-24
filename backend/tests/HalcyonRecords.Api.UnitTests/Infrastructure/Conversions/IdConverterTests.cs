ecab3a50c7f3bbe81e53adf51f032f8e1e3fbb2b
Chris <211069418+chris-mech@users.noreply.github.com>
2026-07-24 16:38:49 +0100

    test: add ID converter round-trip tests

---
+++ backend/tests/HalcyonRecords.Api.UnitTests/Infrastructure/Conversions/IdConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using HalcyonRecords.Api.Domain.Ids;
+using HalcyonRecords.Api.Infrastructure.Conversions;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace HalcyonRecords.Api.UnitTests.Infrastructure.Conversions;
+
+public class IdConverterTests
+{
+    [Fact]
+    public void AlbumIdConverter_RoundTripsThroughProviderValue() =>
+        AssertRoundTrips(new AlbumIdConverter(), new AlbumId(33));
+
+    [Fact]
+    public void ArtistIdConverter_RoundTripsThroughProviderValue() =>
+        AssertRoundTrips(new ArtistIdConverter(), new ArtistId(33));
+
+    [Fact]
+    public void GenreIdConverter_RoundTripsThroughProviderValue() =>
+        AssertRoundTrips(new GenreIdConverter(), new GenreId(33));
+
+    [Fact]
+    public void StockIdConverter_RoundTripsThroughProviderValue() =>
+        AssertRoundTrips(new StockIdConverter(), new StockId(33));
+
+    private static void AssertRoundTrips<TId>(ValueConverter converter, TId id)
+    {
+        var providerValue = converter.ConvertToProvider(id);
+        providerValue.Should().Be(33);
+
+        var roundTripped = converter.ConvertFromProvider(providerValue);
+        roundTripped.Should().Be(id);
+    }
+}
