c5cf7e1de3cd7d8caf7c403a21a9f7c0f115d68d
Chris <211069418+chris-mech@users.noreply.github.com>
2026-07-25 12:48:11 +0100

    test: adopt AutoFixture for test data generation

--- backend/tests/HalcyonRecords.Api.UnitTests/Infrastructure/Conversions/IdConverterTests.cs
+++ backend/tests/HalcyonRecords.Api.UnitTests/Infrastructure/Conversions/IdConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture.Xunit3;
+using FluentAssertions;
 using HalcyonRecords.Api.Domain.Ids;
 using HalcyonRecords.Api.Infrastructure.Conversions;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
@@ -7,22 +8,22 @@ namespace HalcyonRecords.Api.UnitTests.Infrastructure.Conversions;
 
 public class IdConverterTests
 {
-    [Fact]
-    public void AlbumIdConverter_RoundTripsThroughProviderValue() =>
-        AssertRoundTrips(new AlbumIdConverter(), new AlbumId(33));
+    [Theory, AutoData]
+    public void AlbumIdConverter_RoundTripsThroughProviderValue(AlbumId id) =>
+        AssertRoundTrips(new AlbumIdConverter(), id, id.Value);
 
-    [Fact]
-    public void ArtistIdConverter_RoundTripsThroughProviderValue() =>
-        AssertRoundTrips(new ArtistIdConverter(), new ArtistId(33));
+    [Theory, AutoData]
+    public void ArtistIdConverter_RoundTripsThroughProviderValue(ArtistId id) =>
+        AssertRoundTrips(new ArtistIdConverter(), id, id.Value);
 
-    [Fact]
-    public void GenreIdConverter_RoundTripsThroughProviderValue() =>
-        AssertRoundTrips(new GenreIdConverter(), new GenreId(33));
+    [Theory, AutoData]
+    public void GenreIdConverter_RoundTripsThroughProviderValue(GenreId id) =>
+        AssertRoundTrips(new GenreIdConverter(), id, id.Value);
 
-    private static void AssertRoundTrips<TId>(ValueConverter converter, TId id)
+    private static void AssertRoundTrips<TId>(ValueConverter converter, TId id, int expectedProviderValue)
     {
         var providerValue = converter.ConvertToProvider(id);
-        providerValue.Should().Be(33);
+        providerValue.Should().Be(expectedProviderValue);
 
         var roundTripped = converter.ConvertFromProvider(providerValue);
         roundTripped.Should().Be(id);
