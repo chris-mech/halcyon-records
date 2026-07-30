1dc5944b955d9ea47f96c94907a5048680a6005c
Chris <211069418+chris-mech@users.noreply.github.com>
2026-07-30 14:43:21 +0100

    refactor: add SeedToolUserAgent helper

---
+++ backend/tools/HalcyonRecords.SeedDataGenerator.Core/Common/SeedToolUserAgent.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace HalcyonRecords.SeedDataGenerator.Core.Common;
+
+public static class SeedToolUserAgent
+{
+    private static readonly string AppVersion = Assembly.GetExecutingAssembly().GetName().Version
+        is { } version
+        ? $"{version.Major}.{version.Minor}"
+        : "0.0";
+
+    public static string For(string contactEmail) =>
+        $"HalcyonRecordsSeedTool/{AppVersion} ({contactEmail})";
+}
