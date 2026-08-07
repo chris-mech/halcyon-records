d087a82ec69cba6a75ba2f966f36fb71c7471a78
Chris <211069418+chris-mech@users.noreply.github.com>
2026-08-07 18:06:01 +0100

    test: add unit tests for Slugifier

---
+++ backend/tests/HalcyonRecords.Api.UnitTests/Common/SlugifierTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using HalcyonRecords.Shared;
+
+namespace HalcyonRecords.Api.UnitTests.Common;
+
+public class SlugifierTests
+{
+    [Theory]
+    [InlineData("Full Detail Album", "full-detail-album")]
+    [InlineData("Rock & Roll", "rock-and-roll")]
+    [InlineData("HELLO World", "hello-world")]
+    [InlineData("Album   100", "album-100")]
+    [InlineData("  Leading And Trailing Spaces  ", "leading-and-trailing-spaces")]
+    [InlineData("Hyphen---Already--Here", "hyphen-already-here")]
+    [InlineData("Déjà Vu", "déjà-vu")]
+    [InlineData("你好 世界", "你好-世界")]
+    [InlineData("こんにちは 世界", "こんにちは-世界")]
+    [InlineData("コーヒー", "コーヒー")]
+    [InlineData("안녕하세요 세계", "안녕하세요-세계")]
+    [InlineData("Straße", "straße")]
+    [InlineData("!!!", "")]
+    [InlineData("", "")]
+    [InlineData("&", "and")]
+    [InlineData("Foo&Bar", "foo-and-bar")]
+    [InlineData("Track_01", "track-01")]
+    public void Slugify_ProducesExpectedSlug(string input, string expected) =>
+        Slugifier.Slugify(input).Should().Be(expected);
+}
