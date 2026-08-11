a1c004138efc341293eca4d35b8dbbfd8bc9cda7
Chris <211069418+chris-mech@users.noreply.github.com>
2026-08-11 10:09:37 +0100

    chore(backend): remove smoke-test endpoint, superseded by the album detail page

--- backend/src/HalcyonRecords.Api/Features/SmokeTest/SmokeTestQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using ErrorOr;
-using MediatR;
-
-namespace HalcyonRecords.Api.Features.SmokeTest;
-
-public sealed record SmokeTestQuery(bool ShouldFail) : IRequest<ErrorOr<SmokeTestResponse>>;
-
-public sealed record SmokeTestResponse(string Message);
