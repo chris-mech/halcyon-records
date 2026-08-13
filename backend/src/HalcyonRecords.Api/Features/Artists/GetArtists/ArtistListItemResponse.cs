04600539b8a952b28c7ae709a387984c47d89e01
Chris <211069418+chris-mech@users.noreply.github.com>
2026-08-13 22:10:37 +0100

    feat(backend): add GetArtists endpoint

---
+++ backend/src/HalcyonRecords.Api/Features/Artists/GetArtists/ArtistListItemResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace HalcyonRecords.Api.Features.Artists.GetArtists;
+
+public sealed record ArtistListItemResponse(
+    string Sqid,
+    string Name,
+    string NameSlug,
+    int AlbumCount
+);
