c256ee06559b802d5500743f65692422d6d5a869
Chris <211069418+chris-mech@users.noreply.github.com>
2026-08-27 13:41:29 +0100

    fix(backend): track TagDescriptionDocumentTransformer and AlbumsPaginationOptions

---
+++ backend/src/HalcyonRecords.Api/Features/Albums/GetAlbums/AlbumsPaginationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace HalcyonRecords.Api.Features.Albums.GetAlbums;
+
+public sealed class AlbumsPaginationOptions
+{
+    public const string SectionName = "AlbumsPagination";
+
+    public int MinPage { get; init; } = 1;
+
+    public int MinPageSize { get; init; } = 1;
+
+    public int MaxPageSize { get; init; } = 50;
+
+    public int DefaultPageSize { get; init; } = 12;
+}
